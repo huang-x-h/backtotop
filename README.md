edab783d72121f79a376497872d41d9ad24e3f0e
huang.xinghui <huang.x.hui@gmail.com>
2016-07-09 23:00:00 +0800

    first init

---
+++ README.md
@@ -0,0 +1,27 @@
+# Back to top
+
+Back to top jQuery Component
+
+<img src="screenshot.gif" width="384">
+
+## Install
+
+## Usage
+
+import `css/js` files
+
+```html
+<link rel="stylesheet" href="./dist/backtotop.css">
+<script src="./dist/backtotop.js"></script>
+```
+
+then invoke
+
+```js
+$.backtotop(element)
+```
+
+## API
+
+- `$.backtotop(element)`
+  - element element or jQuery element
---
+++ dist/backtotop.js
@@ -0,0 +1,50 @@
+$.backtotop = function(element) {
+  function Debouncer(callback) {
+    this.callback = callback
+    this.ticking = false
+  }
+
+  Debouncer.prototype = {
+    update: function() {
+      this.callback && this.callback()
+      this.ticking = false
+    },
+
+    requestTick: function() {
+      if (!this.ticking) {
+        requestAnimationFrame(this.rafCallback || (this.rafCallback = $.proxy(this.update, this)))
+      }
+
+      this.ticking = true
+    },
+
+    // EventListener implement
+    handleEvent: function() {
+      this.requestTick()
+    }
+  }
+
+  var $el = $(element),
+    offset = 100
+
+  var debouncer = new Debouncer(function() {
+    if ($(window).scrollTop() > offset) {
+      $el.addClass('show')
+    } else {
+      $el.removeClass('show')
+    }
+  })
+
+  window.addEventListener('scroll', debouncer, false)
+  debouncer.handleEvent()
+
+  $el.on('click', function() {
+    // 1 second of animation time
+    // html works for FFX but not Chrome
+    // body works for Chrome but not FFX
+    // This strange selector seems to work universally
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000)
+  })
+}
